7d53a5dac4e0e7ed56e0ef6026207ed8a721afcd
Luca <168549994+lucafml@users.noreply.github.com>
2024-10-24 22:00:57 +0200

    Added some comments

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,15 @@
 var slider = document.getElementById("password-length-slider");
 var currentLength = document.getElementById("password-length-user-input");
 
+
+/* This is a function to change the value from
+a paragraph next to the slider to the current 
+sliders value */
 function changeValue() {
   currentLength.textContent = slider.value;
 }
 
+/* An event listener which constantly calls 
+the changeValue() function with every change 
+on the slider */
 slider.addEventListener("input", changeValue);
